9c12f2050d886e639d18abcc877d14bf4e628b6a
Marcel Pi <169088301+MarcelPiNacy-CVC@users.noreply.github.com>
2025-04-16 11:07:40 +0200

    Revert "Remove references to deprecated Eclipse Ninja generator."
    This reverts commit 8f2656be46b1eaabe4a2f6909a3268099a2b0814.

--- Util/BuildTools/BuildLibCarla.sh
+++ Util/BuildTools/BuildLibCarla.sh
@@ -180,7 +180,7 @@ function build_libcarla {
   if ${NEEDS_CMAKE} ; then
 
     cmake \
-        -G Ninja \
+        -G "Eclipse CDT4 - Ninja" \
         -DCMAKE_BUILD_TYPE=${BUILD_TYPE:-$1} \
         -DLIBCARLA_BUILD_DEBUG=${M_DEBUG} \
         -DLIBCARLA_BUILD_RELEASE=${M_RELEASE} \
